6849e839de1ca4c47f4cd29555a20c8acd6a3f6f
hirata <hirata2024@s.okayama-u.ac.jp>
2025-01-02 00:06:35 +0900

    スケジューラにジョブを停止・再開させる機能を追加

--- scheduler/sched.py
+++ scheduler/sched.py
@@ -1,32 +1,84 @@
 #!/usr/bin/python3  
 
 import socket
+import os
+import errno
+import signal
+import time
 
-M_SIZE = 1024
+# PID を受け取り，その PID を持つプロセスにシグナル送信
+def send_signal(pid, signal):
+    try:
+        os.kill(int(pid), signal)
+    except OSError as e:
+        if e.errno != errno.ESRCH: #TODO: リファクタリング
+            raise e
 
-# 
-host = '127.0.0.1'
-port = 8890
+def suspend_process(pid):
+    try:
+        send_signal(pid, signal.SIGSTOP)
+        print("Process has been suspended.")
 
-locaddr = (host, port)
+        # SIGSTOP送信時のタイムスタンプを取得
+        #with open("sigstop.log","a") as f:
+        #    print("{}".format(time.time()), file=f)
+    
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
-# ①ソケットを作成する
-sock = socket.socket(socket.AF_INET, type=socket.SOCK_DGRAM)
-print('create socket')
+def restart_process(pid):
+    try:
+        send_signal(pid, signal.SIGCONT)
+        print("Process has been restarted.")
+    
+        # SIGCONT送信時のタイムスタンプを取得
+        #with open("sigcont.log","a") as f:
+        #    print("{}".format(time.time()), file=f)
 
-# ②自ホストで使用するIPアドレスとポート番号を指定
-sock.bind(locaddr)
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
-while True:
-    try :
-        # hogeからジョブ状態が送られてくるのを待つ
-        print('Waiting message')
-        message, cli_addr = sock.recvfrom(M_SIZE)
-        message = message.decode(encoding='utf-8')
-        print(f'Received message is [{message}]')
+def control_process(pid):
+    # ここでGPUプログラムの停止・再開を行う
+    suspend_process(pid)
+    time.sleep(3)
+    restart_process(pid)
 
-        # ジョブ状態を受け取ったら，該当ジョブにシグナルを送信し，再度 hoge からの通知を待つ
+def main():
+    M_SIZE = 1024
+    
+    # 
+    host = '127.0.0.1'
+    port = 8890
+    
+    locaddr = (host, port)
+    
+    # ①ソケットを作成する
+    sock = socket.socket(socket.AF_INET, type=socket.SOCK_DGRAM)
+    print('create socket')
+    
+    # ②自ホストで使用するIPアドレスとポート番号を指定
+    sock.bind(locaddr)
+    
+    while True:
+        try :
+            # hogeからジョブ状態が送られてくるのを待つ
+            print('Waiting message')
+            state, cli_addr = sock.recvfrom(M_SIZE)
+            state = state.decode(encoding='utf-8')
+            print(f'Received message is [{state}]')
+            pid = int(state[0])
+    
+            # ジョブ状態を受け取ったら，該当ジョブにシグナルを送信し，再度 hoge からの通知を待つ
+            # ジョブを一時停止，ある程度時間が経過したら再開
+            # ジョブを識別する PID はこのタイミングでもらう
+            control_process(pid)
+            print('Completed job switching')
+    
+    
+        except KeyboardInterrupt:
+            print ('\n . . .\n')
+            sock.close()
 
-    except KeyboardInterrupt:
-        print ('\n . . .\n')
-        sock.close()
+if __name__ == '__main__':
+    main()
